1335c8758b3ce1d4148f704a9c284d1c18e58281
Trey <treycluff@gmail.com>
2023-10-11 19:43:54 -0400

    basic layout

---
+++ src/lib/ImagePicker.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let src: string = "https://picsum.photos/60";
+    export let pickable: boolean = true;
+    export let selected: number = 0;
+</script>
+<main>
+    <img src={src} alt="An anime face" />
+    {#if pickable}
+        <select bind:value={selected} id="images" name="images">
+            <option value="0">Image A</option>
+            <option value="1">Image B</option>
+            <option value="2">Image C</option>
+            <option value="3">Image D</option>
+            <option value="4">Image E</option>
+        </select>
+    {/if}
+</main>
+
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        place-items: center;
+    }
+    img {
+        border-radius: 1rem;
+        width: 100%;
+        height: 100%;
+        image-rendering: pixelated;
+        box-shadow: 1rem 1rem 1rem black;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import ImagePicker from "$lib/ImagePicker.svelte";
+</script>
+
+<main>
+    <!--1st row-->
+    <header></header>
+    <!--2nd row-->
+    <div />
+    <ImagePicker />
+    <div>arrow pointing down</div>
+    <ImagePicker />
+    <div />
+    <!--3rd row-->
+    <div />
+    <div />
+    <ImagePicker pickable={false} />
+    <div />
+    <div />
+
+</main>
+
+<style>
+    main {
+        display: grid;
+        height: 100vh;
+        grid-template-columns: 1fr repeat(3, 2.66fr) 1fr;
+        grid-template-rows: 1fr 4fr 5fr;
+    }
+
+    header {
+        grid-column: 1 / -1;
+        grid-row: 1;
+        background-color: #333;
+    }
+</style>
